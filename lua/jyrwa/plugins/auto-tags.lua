1dff18b5438395a54394504bea37814dae12118d
jyrwaJR <jyrwaboys@gmail.com>
2024-08-04 18:54:58 +0530

    change indent line

---
+++ lua/jyrwa/plugins/auto-tags.lua
@@ -0,0 +1,16 @@
+return {
+  "windwp/nvim-ts-autotag",
+  config = function()
+    require("nvim-ts-autotag").setup({
+      enable_close = true, -- Auto close tags
+      enable_rename = true, -- Auto rename pairs of tags
+      enable_close_on_slash = true, -- Auto close on trailing </
+      filetypes = {
+        -- Override settings for specific filetypes
+        html = {
+          enable_close = true, -- Disable auto close for HTML
+        },
+      },
+    })
+  end,
+}
