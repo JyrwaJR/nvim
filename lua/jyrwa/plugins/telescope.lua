36d9f1a2bbf70d1e7d9c2d35c6ffe0e1a8fbf50d
jyrwaJR <jyrwaboys@gmail.com>
2024-09-10 19:15:26 +0530

    add find git in telecope

--- lua/jyrwa/plugins/telescope.lua
+++ lua/jyrwa/plugins/telescope.lua
@@ -44,6 +44,7 @@ return {
 
     telescope.load_extension("fzf")
     telescope.load_extension("noice")
+    telescope.load_extension("lazygit")
     -- set keymaps
     local keymap = vim.keymap -- for conciseness
     keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
@@ -51,7 +52,7 @@ return {
     keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
     keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
-    -- open noice
     keymap.set("n", "<leader>fn", "<cmd>Telescope noice<cr>", { desc = "Fuzzy find noice" })
+    keymap.set("n", "<leader>fg", "<cmd>Telescope lazygit<cr>", { desc = "Fuzzy find lazygit" })
   end,
 }
